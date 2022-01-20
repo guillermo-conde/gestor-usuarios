efbb7d7a993a20f51b0b66b3cad2ce66f56df80b
Guillermo Conde Flores <condeguillermo8@gmail.com>
2022-01-19 20:00:51 -0600

    Asignando efecto de tarjeta al formulario

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import useFormulario from './hooks/useFormulario';
 import Input from './components/Input';
+import Card from './components/Card';
+import Container from './components/Container';
 
 function App() {
   const [formulario, handleChanges] = useFormulario({nombre:'', apellido:''});
   console.log(formulario);
   return (
-    <form>
-      <Input label='Nombre' 
-        name='nombre' 
-        value={formulario.nombre} 
-        onChange={handleChanges} 
-        placeholder='Nombre'/>
+    <Container>
+      <Card>
+        <div style={{padding:20}}>
+          <form>
+            <Input label='Nombre' 
+              name='nombre' 
+              value={formulario.nombre} 
+              onChange={handleChanges} 
+              placeholder='Nombre'/>
 
-      <Input label='Apellido'   
-        name='apellido' 
-        value={formulario.apellido} 
-        onChange={handleChanges} 
-        placeholder='apellido'/>
-    </form>
+            <Input label='Apellido'   
+              name='apellido' 
+              value={formulario.apellido} 
+              onChange={handleChanges} 
+              placeholder='apellido'/>
+          </form>
+        </div>
+      </Card>
+    </Container>
+    
   );
 }
 
